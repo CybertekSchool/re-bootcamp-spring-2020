117f606ed226bc2cc3fc5c4c664bc9773b1d0c24
marufjont <marufjont@gmail.com>
2020-05-19 12:12:40 -0400

    Sample cucumber project

---
+++ selenium-cucumber-project/src/test/java/com/cybertek/Stepdefs.java
@@ -0,0 +1,11 @@
+package com.cybertek;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+
+}
