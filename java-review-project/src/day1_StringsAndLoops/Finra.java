33a051ff14591d09f6e444f214c10fb094716c22
saimCybertek <saim@cybertekschool.com>
2020-05-20 13:52:43 -0500

    Day 1 - Java - Strings and Loops

--- java-review-project/src/day1_StringsAndLoops/Finra.java
+++ java-review-project/src/day1_StringsAndLoops/Finra.java
@@ -4,6 +4,25 @@ public class Finra {
 
     public static void main(String[] args) {
 
+        for(int i=1; i <= 30; i++) {
+
+           String print = "";
+
+           if(i % 3 == 0) print = "FIN";
+
+           if(i % 5 == 0) print += "RA";
+
+           if(print.isEmpty()){
+               System.out.println(i);
+           } else {
+               System.out.println(print);
+           }
+
+
+        }
+
+
+
     }
 
 }
