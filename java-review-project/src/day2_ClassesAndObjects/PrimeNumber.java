4f8bae0a17ec9505382b1170fcf62c5a2646bd8c
saimCybertek <saim@cybertekschool.com>
2020-05-21 14:54:22 -0500

    Day 2 - Java - Custom classes and object

--- java-review-project/src/day2_ClassesAndObjects/PrimeNumber.java
+++ java-review-project/src/day2_ClassesAndObjects/PrimeNumber.java
@@ -2,4 +2,32 @@ package day2_ClassesAndObjects;
 
 public class PrimeNumber {
 
+    public static void main(String[] args) {
+        printAllTo(100);
+    }
+
+    public static void printAllTo(int num) {
+
+        for(int i = 2; i <= num; i++) {
+
+            if(isPrime(i))
+                System.out.println(i);
+
+
+
+        }
+
+    }
+
+    public static boolean isPrime(int number) {
+
+        for(int i=2; i < number; i++) {
+
+            if(number % i == 0) return false;
+
+        }
+
+        return true;
+    }
+
 }
