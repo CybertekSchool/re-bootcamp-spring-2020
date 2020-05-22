b9b9fccefc49be984689131b494e22573fde7b35
saimCybertek <saim@cybertekschool.com>
2020-05-22 14:01:40 -0500

    Day 2 - Java - Custom classes and object

---
+++ java-review-project/src/day3_OOP/Animal.java
@@ -0,0 +1,15 @@
+package day3_OOP;
+
+public class Animal {
+
+    String name;
+
+//    public Animal() {
+//
+//    }
+
+    public Animal(int i) {
+
+    }
+
+}
---
+++ java-review-project/src/day3_OOP/Dog.java
@@ -0,0 +1,16 @@
+package day3_OOP;
+
+public class Dog extends Animal {
+
+    // Dog is a Animal
+    // Dog has a name
+
+    public Dog() {
+        super(6);
+    }
+
+    public Dog(int i) {
+       super(i)
+    }
+
+}
