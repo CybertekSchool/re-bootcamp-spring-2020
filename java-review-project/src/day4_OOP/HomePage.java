4b6526b94a383582b42de457b280aeb2b79f06a4
saimCybertek <saim@cybertekschool.com>
2020-05-26 13:50:07 -0500

    Day 4 - Java - OOP

---
+++ java-review-project/src/day4_OOP/HomePage.java
@@ -0,0 +1,13 @@
+package day4_OOP;
+
+public class HomePage extends NavBar{
+
+    @Override
+    public void clickNext() {
+        System.out.println("Go to store page");
+    }
+
+    // Elements for HomePage
+
+
+}
---
+++ java-review-project/src/day4_OOP/NavBar.java
@@ -0,0 +1,7 @@
+package day4_OOP;
+
+public abstract class NavBar {
+
+    public abstract void clickNext();
+
+}
