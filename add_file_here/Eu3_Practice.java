d2b3b2e4b667ad04d13ceaee17f3fe4544aa4310
Durmaz68 <erdurmaz90@gmail.com>
2024-03-01 08:47:52 +0000

    Added practice file

---
+++ add_file_here/Eu3_Practice.java
@@ -0,0 +1,19 @@
+package add_file_here;
+
+public class Eu3_Practice {
+    public static void main(String[] args) {
+        System.out.println("EU3 is opening pull request");
+        //Commit your change
+        // with a nice commit message
+        // push it back to GitHub.
+        // Observe the change
+        // from your GitHub repo.
+        // Click on the compare
+        // create a pull request(PR)
+        // to asl original repo owner
+        // to merge your change
+
+
+
+    }
+}
